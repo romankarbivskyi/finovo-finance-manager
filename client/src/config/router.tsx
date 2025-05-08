fd76da9e80663a4ea214d56d574a73ef5745a98a
Roman Karbivskyi <vt231_krp@student.ztu.edu.ua>
2025-05-08 16:37:59 +0300

    feat: create intuitive folder structure

---
+++ client/src/config/router.tsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router";
+import { HomePage } from "../pages";
+import { DashboardLayout } from "../layouts";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout />,
+    children: [],
+  },
+]);
---
+++ client/src/layouts/DashboardLayout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router";
+
+const DashboardLayout = () => {
+  return (
+    <div>
+      DashboardLaout
+      <Outlet />
+    </div>
+  );
+};
+
+export default DashboardLayout;
