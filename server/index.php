3d1b8c6e52a05419c5c0a05415eca1141f67392f
Roman Karbivskyi <vt231_krp@student.ztu.edu.ua>
2025-05-09 19:40:37 +0300

    refactor: remove comment

--- server/index.php
+++ server/index.php
@@ -12,7 +12,7 @@ if (in_array($origin, $allowed_origins)) {
   header("Access-Control-Allow-Origin: 'http://localhost:5173'");
 }
 
-header("Access-Control-Allow-Credentials: true"); // This is required for credentials
+header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
