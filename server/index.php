0496aad5d0c574187082c8ae03634d2bd60f8d85
Roman Karbivskyi <vt231_krp@student.ztu.edu.ua>
2025-05-09 16:28:50 +0300

    fix: cors

--- server/index.php
+++ server/index.php
@@ -1,5 +1,14 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  http_response_code(204);
+  exit();
+}
+
 require_once __DIR__ . '/autoload.php';
 
 use server\Core\Database;
