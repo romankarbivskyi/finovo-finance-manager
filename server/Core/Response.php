4d06cb34669b048392acfeb1f0b196c96c7984a3
Roman Karbivskyi <vt231_krp@student.ztu.edu.ua>
2025-05-10 12:11:07 +0300

    feat: add success status for api response

--- server/Core/Response.php
+++ server/Core/Response.php
@@ -8,6 +8,7 @@ class Response
   {
     http_response_code($statusCode);
     header('Content-Type: application/json');
+    $data['success'] = $statusCode >= 200 && $statusCode < 300;
     echo json_encode($data, JSON_PRETTY_PRINT);
     exit;
   }
