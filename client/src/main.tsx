0ce4d22225939996b7a620f4bab2238bb75caad7
Roman Karbivskyi <vt231_krp@student.ztu.edu.ua>
2025-05-11 08:37:20 +0300

    feat: setup tanstack query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,16 @@ import { RouterProvider } from "react-router";
 import { router } from "./config/router";
 import { AuthProvider } from "./contexts/auth";
 import { RootLayout } from "./layouts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <AuthProvider>
-    <RootLayout>
-      <RouterProvider router={router} />
-    </RootLayout>
-  </AuthProvider>,
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <RootLayout>
+        <RouterProvider router={router} />
+      </RootLayout>
+    </AuthProvider>
+  </QueryClientProvider>,
 );
