d82689b7213bd5cb33b0698ec83f104371d6bc72
Roman Karbivskyi <vt231_krp@student.ztu.edu.ua>
2025-05-15 19:28:41 +0300

    feat: display all user transactions on transactions page

--- client/src/pages/TransactionsPage.tsx
+++ client/src/pages/TransactionsPage.tsx
@@ -1,42 +1,43 @@
-// import { TransactionList } from "@/components";
-// import Header from "@/components/Header";
-// import { ITEMS_PER_PAGE } from "@/constants";
-// import { getAllTransactions } from "@/services/transaction.service";
-// import { useQuery } from "@tanstack/react-query";
-// import { useState } from "react";
+import { TransactionList } from "@/components";
+import Header from "@/components/Header";
+import { ITEMS_PER_PAGE } from "@/constants";
+import { getAllTransactions } from "@/services/transaction.service";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 
 const TransactionsPage = () => {
-  // const [page, setPage] = useState<number>(1);
-  // const offset = (page - 1) * ITEMS_PER_PAGE;
+  const [page, setPage] = useState<number>(1);
+  const offset = (page - 1) * ITEMS_PER_PAGE;
 
-  // const { data: apiResponse, isLoading } = useQuery({
-  //   queryKey: ["transactions", page],
-  //   queryFn: async () => await getAllTransactions(ITEMS_PER_PAGE, offset),
-  // });
+  const {
+    data: apiResponse,
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["transactions", page],
+    queryFn: async () => await getAllTransactions(ITEMS_PER_PAGE, offset),
+  });
 
-  // const transactionsData = apiResponse?.data || [];
-  // const {transactions, total} = transactionsData;
+  const { transactions = [], total = 0 } = apiResponse?.data ?? {};
 
-  // return (
-  //   <div>
-  //     <Header
-  //       title="Transactions"
-  //       subtitle="View and manage your transactions"
-  //     />
-  //     <div className="container mx-auto mt-4">
-  //       <TransactionList
-  //         transactions={transactions}
-  //         total={total}
-  //         currency={apiResponse?.currency || ""}
-  //         isLoading={isLoading}
-  //         page={page}
-  //         setPage={setPage}
-  //       />
-  //     </div>
-  //   </div>
-  // );
-
-  return <div>Transactions</div>;
+  return (
+    <div>
+      <Header
+        title="Transactions"
+        subtitle="View and manage your transactions"
+      />
+      <div className="container mx-auto mt-4">
+        <TransactionList
+          transactions={transactions}
+          total={total}
+          isLoading={isLoading}
+          page={page}
+          setPage={setPage}
+          onDelete={refetch}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default TransactionsPage;
